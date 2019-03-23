764570bb59f382c03b4bfd907236e867ba3e017d
SebastianMeler <sebastianissimus@gmail.com>
2019-03-23 12:32:02 +0100

    bartek files :)

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/CheckpointBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointBehaviour : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int points;
+   
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameManager.CheckpointCollected(gameObject.transform.position, points);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public void CheckpointCollected(Vector3 fireSpot, int pointsCheckpoint) {
+        Instantiate(explosion, fireSpot, Quaternion.identity);
+    }
+
+}
