94edd1b9d02db7ebeba764d9406c1f9abe2df68a
SebastianMeler <sebastianissimus@gmail.com>
2019-03-23 11:59:15 +0100

    pointscoin

--- Assets/Standard Assets/Characters/RollerBall/Scripts/Coin.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int points;
+    public int pointsCoin;
     public ParticleSystem coinParticle;
 
     private void OnTriggerEnter(Collider other)
