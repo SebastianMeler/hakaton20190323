955c90cf0a0373062aa20cad73aec67c57ffbedf
SebastianMeler <sebastianissimus@gmail.com>
2019-03-23 11:47:10 +0100

    coinprefab

---
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int points;
+    public ParticleSystem coinParticle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            var newcoinParticle = Instantiate(coinParticle,
+                                              new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                                              Quaternion.identity);
+        }
+    }
+}
